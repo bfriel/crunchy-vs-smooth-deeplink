f8bd0aa029cf7eb88f04e0640bc333336a7da9d3
Brian Friel <briandfriel@gmail.com>
2022-06-04 17:20:37 -0700

    feat(connect): Decrypt and parse return params

--- App.tsx
+++ App.tsx
@@ -1,15 +1,84 @@
 import "react-native-get-random-values";
+import "react-native-url-polyfill/auto";
+import { Buffer } from "buffer";
+global.Buffer = global.Buffer || Buffer;
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Linking from "expo-linking";
 import nacl from "tweetnacl";
 import bs58 from "bs58";
+import { PublicKey } from "@solana/web3.js";
 
 const onConnectRedirectLink = Linking.createURL("onConnect");
 
+const decryptPayload = (data: string, nonce: string, sharedSecret?: Uint8Array) => {
+  if (!sharedSecret) throw new Error("missing shared secret");
+
+  const decryptedData = nacl.box.open.after(bs58.decode(data), bs58.decode(nonce), sharedSecret);
+  if (!decryptedData) {
+    throw new Error("Unable to decrypt data");
+  }
+  return JSON.parse(Buffer.from(decryptedData).toString("utf8"));
+};
+
 export default function App() {
+  const [deeplink, setDeepLink] = useState<string>("");
   const [dappKeyPair] = useState(nacl.box.keyPair());
+
+  const [sharedSecret, setSharedSecret] = useState<Uint8Array>();
+  const [session, setSession] = useState<string>();
+  const [phantomWalletPublicKey, setPhantomWalletPublicKey] = useState<PublicKey>();
+
+  useEffect(() => {
+    const initializeDeeplinks = async () => {
+      const initialUrl = await Linking.getInitialURL();
+      if (initialUrl) {
+        setDeepLink(initialUrl);
+      }
+    };
+    initializeDeeplinks();
+    const listener = Linking.addEventListener("url", handleDeepLink);
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  const handleDeepLink = ({url}: Linking.EventType) => setDeepLink(url);
+
+  // handle in-bound links
+  useEffect(() => {
+    if (!deeplink) return;
+
+    const url = new URL(deeplink);
+    const params = url.searchParams;
+
+    if (params.get("errorCode")) {
+      console.log("error!")
+      console.log(JSON.stringify(Object.fromEntries([...params]), null, 2));
+      return;
+    }
+
+    if (/onConnect/.test(url.pathname)) {
+      console.log("we're connecting!")
+      console.log(deeplink);
+
+      const sharedSecretDapp = nacl.box.before(
+        bs58.decode(params.get("phantom_encryption_public_key")!),
+        dappKeyPair.secretKey
+      );
+      
+      const connectData = decryptPayload(
+        params.get("data")!,
+        params.get("nonce")!,
+        sharedSecretDapp
+      );
+
+      setSharedSecret(sharedSecretDapp);
+      setSession(connectData.session);
+      setPhantomWalletPublicKey(new PublicKey(connectData.public_key));
+    }
+  }, [deeplink])
 
   const connect = async () => {
     const baseUrl = "https://phantom.app/ul/v1/";
@@ -28,7 +97,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Crunchy vs Smooth</Text>
+      {phantomWalletPublicKey ? 
+      <Text>{`Connected with ${phantomWalletPublicKey}`}</Text>
+      :
       <Button title="Connect" onPress={connect} />
+      }
       <StatusBar style="auto" />
     </View>
   );
