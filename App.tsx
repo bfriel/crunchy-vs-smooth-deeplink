cb0a7776bbe5971c304e86b8f6753e45dab5b641
Brian Friel <briandfriel@gmail.com>
2022-06-13 20:53:26 -0700

    feat(disconnect): show vote account and add disconnect

--- App.tsx
+++ App.tsx
@@ -2,24 +2,50 @@ import "react-native-get-random-values";
 import "react-native-url-polyfill/auto";
 import { Buffer } from "buffer";
 global.Buffer = global.Buffer || Buffer;
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 import * as Linking from "expo-linking";
 import nacl from "tweetnacl";
 import bs58 from "bs58";
 import { PublicKey } from "@solana/web3.js";
+import idl from "./idl.json";
+
+const programID = new PublicKey(idl.metadata.address);
 
 const onConnectRedirectLink = Linking.createURL("onConnect");
+const onDisconnectRedirectLink = Linking.createURL("onDisconnect");
 
-const decryptPayload = (data: string, nonce: string, sharedSecret?: Uint8Array) => {
+const decryptPayload = (
+  data: string,
+  nonce: string,
+  sharedSecret?: Uint8Array
+) => {
   if (!sharedSecret) throw new Error("missing shared secret");
 
-  const decryptedData = nacl.box.open.after(bs58.decode(data), bs58.decode(nonce), sharedSecret);
+  const decryptedData = nacl.box.open.after(
+    bs58.decode(data),
+    bs58.decode(nonce),
+    sharedSecret
+  );
   if (!decryptedData) {
     throw new Error("Unable to decrypt data");
   }
   return JSON.parse(Buffer.from(decryptedData).toString("utf8"));
+};
+
+const encryptPayload = (payload: any, sharedSecret?: Uint8Array) => {
+  if (!sharedSecret) throw new Error("missing shared secret");
+
+  const nonce = nacl.randomBytes(24);
+
+  const encryptedPayload = nacl.box.after(
+    Buffer.from(JSON.stringify(payload)),
+    nonce,
+    sharedSecret
+  );
+
+  return [nonce, encryptedPayload];
 };
 
 export default function App() {
@@ -28,7 +54,16 @@ export default function App() {
 
   const [sharedSecret, setSharedSecret] = useState<Uint8Array>();
   const [session, setSession] = useState<string>();
-  const [phantomWalletPublicKey, setPhantomWalletPublicKey] = useState<PublicKey>();
+  const [phantomWalletPublicKey, setPhantomWalletPublicKey] =
+    useState<PublicKey | null>(null);
+
+  const [voteAccount, setVoteAccount] = useState<{
+    account: PublicKey | null;
+    accountBump: Number | null;
+  }>({
+    account: null,
+    accountBump: null,
+  });
 
   useEffect(() => {
     const initializeDeeplinks = async () => {
@@ -44,7 +79,7 @@ export default function App() {
     };
   }, []);
 
-  const handleDeepLink = ({url}: Linking.EventType) => setDeepLink(url);
+  const handleDeepLink = ({ url }: Linking.EventType) => setDeepLink(url);
 
   // handle in-bound links
   useEffect(() => {
@@ -54,20 +89,17 @@ export default function App() {
     const params = url.searchParams;
 
     if (params.get("errorCode")) {
-      console.log("error!")
+      console.log("error!");
       console.log(JSON.stringify(Object.fromEntries([...params]), null, 2));
       return;
     }
 
     if (/onConnect/.test(url.pathname)) {
-      console.log("we're connecting!")
-      console.log(deeplink);
-
       const sharedSecretDapp = nacl.box.before(
         bs58.decode(params.get("phantom_encryption_public_key")!),
         dappKeyPair.secretKey
       );
-      
+
       const connectData = decryptPayload(
         params.get("data")!,
         params.get("nonce")!,
@@ -77,31 +109,70 @@ export default function App() {
       setSharedSecret(sharedSecretDapp);
       setSession(connectData.session);
       setPhantomWalletPublicKey(new PublicKey(connectData.public_key));
+    } else if (/onDisconnect/.test(url.pathname)) {
+      setPhantomWalletPublicKey(null);
     }
-  }, [deeplink])
+  }, [deeplink]);
+
+  useEffect(() => {
+    const getVoteAccount = async () => {
+      let [account, accountBump] = await PublicKey.findProgramAddress(
+        [Buffer.from("vote_account")],
+        programID
+      );
+      setVoteAccount({ account, accountBump });
+    };
+    getVoteAccount();
+  }, []);
 
   const connect = async () => {
     const baseUrl = "https://phantom.app/ul/v1/";
     const params = new URLSearchParams({
       dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
       cluster: "mainnet-beta",
-      app_url: "https://phantom.app",
+      app_url: "https://pbvote.com/",
       redirect_link: onConnectRedirectLink,
     });
 
-    const myUrl = `${baseUrl}connect?${params.toString()}`;
-    
-    Linking.openURL(myUrl);
+    const url = `${baseUrl}connect?${params.toString()}`;
+
+    Linking.openURL(url);
+  };
+
+  const disconnect = async () => {
+    const payload = {
+      session,
+    };
+
+    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
+
+    const baseUrl = "https://phantom.app/ul/v1/";
+
+    const params = new URLSearchParams({
+      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
+      nonce: bs58.encode(nonce),
+      redirect_link: onDisconnectRedirectLink,
+      payload: bs58.encode(encryptedPayload),
+    });
+
+    const url = `${baseUrl}disconnect?${params.toString()}`;
+
+    Linking.openURL(url);
   };
 
   return (
     <View style={styles.container}>
       <Text>Crunchy vs Smooth</Text>
-      {phantomWalletPublicKey ? 
-      <Text>{`Connected with ${phantomWalletPublicKey}`}</Text>
-      :
-      <Button title="Connect" onPress={connect} />
-      }
+      {phantomWalletPublicKey ? (
+        <>
+          <Text>{`Connected with ${phantomWalletPublicKey}`}</Text>
+          <Text>{`Account ${voteAccount.account}`}</Text>
+          <Text>{`Account Bump ${voteAccount.accountBump}`}</Text>
+          <Button title="Disconnect" onPress={disconnect} />
+        </>
+      ) : (
+        <Button title="Connect" onPress={connect} />
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -110,8 +181,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
